23aa82a7a03efa78ba3f94fbdcf1d8bee875cdc6
Ho-Ling Poon <holingpoon@nypl.org>
2017-10-25 12:32:50 -0400

    REQ-326 Unit Testing Setup
    * Adding Travis CI and Coveralls.
    * Adding phpunit, coveralls and codesniffer.
    * Updating phpunit settings to look for correct directory for tests.
    * Adding first scaffold of unit test.

---
+++ tests/Controller/PatronControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace NYPL\Services\Test\Controller;
+
+use NYPL\Services\Controller\PatronController;
+use PHPUnit\Framework\TestCase;
+
+class PatronControllerTest extends TestCase
+{
+    public $fakePatronController;
+
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+}
